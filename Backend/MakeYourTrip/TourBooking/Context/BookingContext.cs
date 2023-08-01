b132f9e395ef28d2fd554ff5c473f54870aba66b
Abinandhan <Windows@gmail.com>
2023-08-01 21:33:27 +0530

    Backend Booking Completed

---
+++ Backend/MakeYourTrip/TourBooking/Context/BookingContext.cs
@@ -0,0 +1,15 @@
+﻿using BookingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TourBooking.Context
+{
+    public class BookingContext : DbContext
+    {
+        public BookingContext(DbContextOptions<BookingContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+    }
+}
