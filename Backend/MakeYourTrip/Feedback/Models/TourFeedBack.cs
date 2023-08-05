5d28b1bbbaa9a6fcee140746070677a4b1e3f6cc
Abinandhan <Windows@gmail.com>
2023-08-05 17:26:20 +0530

    FeedBack API Completed

---
+++ Backend/MakeYourTrip/Feedback/Models/TourFeedBack.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Feedback.Models
+{
+    public class TourFeedBack
+    {
+
+        [Key]
+        public int FeedbackId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string TouristName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string FeedbackText { get; set; }
+
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        [Required]
+        public DateTime DateSubmitted { get; set; }
+    
+    }
+}
